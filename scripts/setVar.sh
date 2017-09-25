f611dac47c2b72b585f53ba5008f7072a2ffe7fe
Atul Malaviya <azooinmyluggage@users.noreply.github.com>
2017-09-25 23:16:34 +0530

    Update setVar.sh

--- scripts/setVar.sh
+++ scripts/setVar.sh
@@ -2,6 +2,6 @@
 mkdir -p /home/tfs/tempout
 echo $1 > /home/tfs/tempout/out.txt
 echo "Perform Operation in su mode"
-export MSG_STR=HelloWorld
+export NODE_ENV=production
 echo "Export MSG_STR=HelloWorld"
 # continue your compilation commands here
