c23ff9d78c4cf637458118c5df9f9d5aa9059008
Atul Malaviya <azooinmyluggage@users.noreply.github.com>
2017-09-25 19:30:53 +0530

    Update setVar.sh

--- scripts/setVar.sh
+++ scripts/setVar.sh
@@ -1,2 +1,7 @@
+#!/bin/sh
 mkdir -p /home/tfs/tempout
 echo $1 > /home/tfs/tempout/out.txt
+echo "Perform Operation in su mode"
+export MSG_STR=HelloWorld
+echo "Export MSG_STR=HelloWorld"
+# continue your compilation commands here
